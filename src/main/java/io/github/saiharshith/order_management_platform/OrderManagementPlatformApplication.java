cdbb253928c38bd2b4fbf6d9c76b15b489f223aa
Chinni Sai Harshith <csaih236081@gmail.com>
2026-08-31 19:47:27 +0530

    Initial Spring Boot skeleton

---
+++ src/main/java/io/github/saiharshith/order_management_platform/OrderManagementPlatformApplication.java
@@ -0,0 +1,13 @@
+package io.github.saiharshith.order_management_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagementPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagementPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/saiharshith/order_management_platform/OrderManagementPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.saiharshith.order_management_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
